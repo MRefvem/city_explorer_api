2cc8b9dc3b8efa3df2da1991ae1a58e37ff319a7
MichaelRefvem <mkrefvem@gmail.com>
2020-06-08 15:32:32 -0700

    revised server.js

--- server.js
+++ server.js
@@ -54,6 +54,45 @@ app.get('*', (request, response) => {
   response.status(404).send('sorry, this route does not exist');
 })
 
+app.get('/weather', (request, response) => {
+  try{
+    // data: { "valid_date": "2020-04-13" },
+    console.log(request.data.valid_date);
+    let valid_date = request.data.valid_date;
+  
+    let weatherData = require('./data/weather.json');
+  
+    let returnObj = new Weather(city_name, weatherData[0]);
+  
+    console.log(returnObj);
+    // let returnObj = {
+    //   search_query: search_query,
+    //   formatted_query: geoData[0].display_name,
+    //   latitude: geoData[0].lat,
+    //   longitude: geoData[0].lon
+    // }
+  
+    response.status(200).send(returnObj);
+    
+  } catch(err){
+    console.log('ERROR', err);
+    response.status(500).send('sorry, we messed up');
+  }
+
+})
+
+function Weather(searchQuery, obj){
+  // we want data.weather.description, data.valid_date
+  this.city_name = searchQuery;
+  // this.formatted_query = obj.display_name;
+  this.data.weather.description = obj.description;
+  this.data.valid_date = obj.date;
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('sorry, this route does not exist');
+})
+
 // turn on the lights - move into the house - start the server
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
