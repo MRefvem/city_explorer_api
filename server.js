9fea700979ffd3636304d5e5a57a56d980fba80d
MichaelRefvem <mkrefvem@gmail.com>
2020-06-11 13:25:00 -0700

    committing changes from class demo, starting to build out functions to handle movies and yelp content

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const superagent = require('superagent');
 require('dotenv').config();
 const pg = require('pg');
+const { query } = require('express');
 
 
 // INITIALIZE APP - ESTABLISH PORT
@@ -38,41 +39,61 @@ app.get('/add', (request, response) => {
 })
 
 
+// CALLING HANDLERS
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/trails', trailsHandler);
+app.get('/yelp', restaurantHandler);
+app.get('/movies', moviesHandler);
+app.use('*', handleNotFound);
+
+
 // API FUNCTIONS
-app.get('/location', (request, response) => {
+function locationHandler(request, response) {
   
-    const city = request.query.city;
-    console.log(city);
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEO_DATA_API_KEY}&q=${city}&format=json`;
-    let sqlQuery = 'SELECT * FROM locations WHERE search_query LIKE ($1);';
-    let safeValue = [city];
+  const city = request.query.city;
+  console.log(city);
+  const url = `https://us1.locationiq.com/v1/search.php`;
+  
+  const queryParams = {
+    key: process.env.GEO_DATA_API_KEY,
+    q: city,
+    format: 'json',
+    limit: 1
+  }
+  
+  let sqlQuery = 'SELECT * FROM locations WHERE search_query LIKE ($1);';
+  let safeValue = [city];
+  
+  client.query(sqlQuery, safeValue) 
+  .then(sqlResults => {
+    console.log(sqlQuery, safeValue);
+    console.log(sqlResults);
+    if (sqlResults.rowCount !== 0){
+      console.log(sqlResults.rows);
+      response.status(200).send(sqlResults.rows[0]);
+    } else {
+      // Check superagent documentation to find out how to set a header
+      superagent.get(url).query(queryParams).then(resultsFromSuperAgent => {
+        console.log('results from superagent', resultsFromSuperAgent.body);
+        const geoData = resultsFromSuperAgent.body[0];
+        let finalObj = new Location(city, geoData);
+        let sqlQuery = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
+        let safeValue = [city, finalObj.formatted_query, finalObj.latitude, finalObj.longitude];
+        
+        client.query(sqlQuery, safeValue);
+        response.status(200).send(finalObj);
+      })
+    }
+  }).catch(err => console.error(err));
+};
 
-    client.query(sqlQuery, safeValue) 
-    .then(sqlResults => {
-        console.log(sqlQuery, safeValue);
-        console.log(sqlResults);
-        if (sqlResults.rowCount !== 0){
-            console.log(sqlResults.rows);
-            response.status(200).send(sqlResults.rows[0]);
-        } else {
-          superagent.get(url).then(resultsFromSuperAgent => {
-            let finalObj = new Location(city, resultsFromSuperAgent.body[0]);
-            let sqlQuery = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
-            let safeValue = [city, finalObj.formatted_query, finalObj.latitude, finalObj.longitude];
-
-            client.query(sqlQuery, safeValue);
-            response.status(200).send(finalObj);
-          })
-        }
-    }).catch(err => console.error(err));
-});
-
-app.get('/weather', (request, response) => {
+function weatherHandler(request, response) {
   try {
     const search_query = request.query.search_query;
     const key = process.env.WEATHER_API_KEY;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${search_query}&key=${key}&days=8`;
-
+    
     superagent.get(url).then(resultsFromSuperAgent => {
       const data = resultsFromSuperAgent.body.data;
       const results = data.map(item => new Weather(item));
@@ -82,25 +103,76 @@ app.get('/weather', (request, response) => {
     console.log('ERROR', err);
     response.status(500).send('sorry, we messed up');
   }    
-});
-
-app.get('/trails', (request, response) => {
+};
+      
+function trailsHandler(request, response) {
   try { 
     const { latitude, longitude } = request.query;
     const key = process.env.TRAIL_API_KEY;
     const url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${key}`;
-
+    
     superagent.get(url)
-      .then(resultsFromSuperAgent => {
-        const data = resultsFromSuperAgent.body.trails;
-        const results = data.map(item => new Trail(item));
-        response.status(200).send(results);
+    .then(resultsFromSuperAgent => {
+      const data = resultsFromSuperAgent.body.trails;
+      const results = data.map(item => new Trail(item));
+      response.status(200).send(results);
     }) 
   } catch(err) {
     console.log('ERROR', err);
     response.status(500).send('sorry, we messed up');
   }
-});
+};
+      
+      
+function restaurantHandler(request, repsonse){
+  console.log('this is our restaurant', request.query);
+
+  const page = request.query.page;
+  const numPerPage = 5;
+  const start = (page - 1) * numPerPage; // this allows us to start with the 1-5 and then 5-10 and then 10-15 etc.
+
+  const url = 'https://api.yelp.com/v3/businesses/search';
+
+  const queryParams = {
+    latitude: request.query.latitude,
+    start: start,
+    limit: numPerPage,
+    longitude: request.query.longitude
+  }
+
+  superagent.get(url)
+    // This next line will be slightly different for Yelp, look up documentation for setting key in header
+    .set('user-key', process.env.YELP_API_KEY)
+    .query(queryParams)
+    .then(data => {
+      console.log('data from superagent', data.body);
+      let restaurantArray = data.body.restaurants;
+      console.log('this is my restaurant array', restaurantArray);
+
+      const finalRestaurants = restaurantArray.map(eatery => {
+        return new Restaurant(eatery);
+      })
+    })
+};
+
+function moviesHandler(request, response) {
+  console.log('this is our movies', request.query);
+  try { 
+    const city = request.query.city;
+    const key = process.env.MOVIE_API_KEY;
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=${key}`;
+    
+    superagent.get(url)
+    .then(resultsFromSuperAgent => {
+      const data = resultsFromSuperAgent.body.trails;
+      const results = data.map(item => new Trail(item));
+      response.status(200).send(results);
+    }) 
+  } catch(err) {
+    console.log('ERROR', err);
+    response.status(500).send('sorry, we messed up');
+}
+
 
 
 // CONSTRUCTORS
@@ -110,7 +182,7 @@ function Location(searchQuery, obj){
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 };
-
+      
 function Weather(obj){
   this.forecast = obj.weather.description;
   this.time = obj.datetime;
@@ -128,12 +200,30 @@ function Trail(obj){
   this.condition_date = obj.conditionDate.slice(0, 10);
   this.condition_time = obj.conditionDate.slice(11);
 };
+      
+function Restaurant(obj){
+  this.name = obj.name;
+  this.image_url = obj.url;
+  this.price = obj.price;
+  this.rating = obj.rating;
+  this.url = obj.url;
+};
 
-
+function Movies(obj){
+  this.title = obj.title;
+  this.overview = obj.overview;
+  this.average_votes = obj.average_votes;
+  this.total_votes = obj.total_votes;
+  this.image._url = obj.image_url;
+  this.popularity = obj.popularity;
+  this.released_on = obj.released_on;
+};
+      
+      
 // 404
-app.get('*', (request, response) => {
+function handleNotFound(request, response) {
   response.status(404).send('sorry, this route does not exist');
-});
+};
 
 
 // CONNECT CLIENT
