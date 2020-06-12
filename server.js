950e8b8d220dff36d3f51583c6fbdd8678f5317b
MichaelRefvem <mkrefvem@gmail.com>
2020-06-11 17:33:59 -0700

    fixed pictures for Yelp

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ function Trail(obj){
       
 function Restaurant(obj){
   this.name = obj.name;
-  this.image_url = obj.url;
+  this.image_url = obj.image_url;
   this.price = obj.price;
   this.rating = obj.rating;
   this.url = obj.url;
